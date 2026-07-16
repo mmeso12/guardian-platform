d9c9a51eef98e7048bdd411d2c978f8d1827f1ea
mmesoma <mmezimmesoma@gmail.com>
2026-07-16 19:27:46 +0200

    Set up Guardian Spring Boot backend

---
+++ backend/src/main/java/com/guardian/cloud/GuardianCloudApplication.java
@@ -0,0 +1,13 @@
+package com.guardian.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardianCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardianCloudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/guardian/cloud/GuardianCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guardian.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardianCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
